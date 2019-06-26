dff9a588e97db78d40934c1be33d0630a76cbdfe
Lindsey Coffee-Johnson <lcoffeejohnson@gmail.com>
2019-06-26 14:39:33 -0500

    Updated Server to recieve Messages continuously

--- Server.java
+++ Server.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 public class Server {
 
-  private Socket socket = null;
   private ServerSocket serverSocket = null;
 
   public Server(int portNum) {
@@ -13,44 +12,44 @@ public class Server {
     try {
       serverSocket = new ServerSocket(portNum);
       System.out.println("Server started...");
-      socket = serverSocket.accept();
-      System.out.println("Connection with client successful!");
-
     } 
     catch(IOException i) {
       System.out.println(i);
     }
   }
 
-  public Socket getSocket() {
-    return socket;
-  }
 
-  public Message recieveMessage() throws IOException, ClassNotFoundException {
+ /* public Message recieveMessage() throws IOException, ClassNotFoundException {
     ObjectInputStream messageFromClient = new ObjectInputStream(socket.getInputStream());
     Message message = (Message) messageFromClient.readObject();
     return message;
-  } 
+  }*/ 
 
   public static void main(String[] args) {
   
     //Create new Server object
     Server server = new Server(50001);
-    
+    Socket socket = null;   
+
     try {
-      System.out.println("Tryng to recieve message....");
-      System.out.println("Recieved message: " + server.recieveMessage());
-    }
-    catch (IOException i) {
+      while (true) {
+        socket = server.serverSocket.accept();
+        ObjectInputStream messageFromClient = new ObjectInputStream(socket.getInputStream());
+        Message message = null;
+        try {
+          message = (Message) messageFromClient.readObject();
+        } catch (ClassNotFoundException c) {
+          System.out.println(c);
+        }
+        System.out.println("Recieved message: " + message);  
+      } 
+    } catch (IOException i) {
       System.out.println(i);
     }
-    catch (ClassNotFoundException c) {
-      System.out.println(c);
-    }
-    
+     
     //Try to close out socket
     try {
-      server.getSocket().close();
+      socket.close();
     }
     catch (IOException i) {
       System.out.println(i);
