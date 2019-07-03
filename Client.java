2b4fcf94080729a2a97010bb52677f106d2744aa
Lindsey Coffee-Johnson <lcoffeejohnson@gmail.com>
2019-07-03 11:37:58 -0500

    Cleaned up Client class

--- Client.java
+++ Client.java
@@ -30,29 +30,4 @@ public class Client {
         return false;
       }
     }
-
-  public static void main(String[] args) {
-  
-    //Create new Client
-    Client client = new Client("localhost", 50001);
-
-    //User user = new User(new Policy(true, true));
-
-    System.out.println("Set up new client");
-
-    //Send Message
-    client.sendMessage(new Message(MessageType.NOTIFY));
-    System.out.println("Message sent!");
-    client.sendMessage(new Message(MessageType.ACKNOWLEDGE));
-    System.out.println("Second message sent!");
-    
-    //Close out client
-   /* try {
-      client.getSocket().close();
-    }
-    catch (IOException i) {
-      System.out.println(i);
-    }*/
-  }
-   
 }
