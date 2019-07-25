6c35beac0ef132d3f37319c18655080a82b75551
Lindsey Coffee-Johnson <lcoffeejohnson@gmail.com>
2019-07-25 17:07:41 -0500

    Made port numbers and addresses for client and server command line arguments

--- User.java
+++ User.java
@@ -12,42 +12,67 @@ import java.io.*;
 public class User {
 
   ArrayList<Integer> hashVals = new ArrayList<Integer>();
-  Policy myPolicy = null;
-  Client myClient = null;
-  UserServer myServer = null;
+  Policy policy = null;
+  Client client = null;
+  UserServer server = null;
 
-  public User(Policy myPolicy) {
-    this.myPolicy = myPolicy;
-    myClient = new Client("localhost", 50001);
-    myServer = new UserServer(50002);
-    myServer.start();
+  public User(Policy policy) {
+    this.policy = policy;
   }
+
+  /**
+   * Creates and starts the User's server and client
+   */
+   public void startServers(int serverPort, String address, int clientPort) {
+     if(clientPort != serverPort) { //Users's server and client cannot use the same port
+       server = new UserServer(serverPort);
+       client = new Client(address, clientPort);
+       server.start();
+     } else {
+       System.out.print("Server and client port numbers cannot match. Client / server setup aborted.");
+     }
+   }
 
   /**
    * Uploads a piece of user data along with the user's
    * policy as a Message to the ServiceProvider
    */
   public boolean uploadData(String data) {
-    StickyHeader stickyHeader = new StickyHeader(data, this.myPolicy);
-    int sHHash = stickyHeader.hashCode(); //Probably not a strong enough hash function
-    hashVals.add(0, sHHash);
-    Message toSend = new Message(MessageType.UPLOAD, stickyHeader, sHHash);
+    if (client != null ) {
+      StickyHeader stickyHeader = new StickyHeader(data, this.policy);
+      int sHHash = stickyHeader.hashCode(); //Probably not a strong enough hash function
+      hashVals.add(0, sHHash);
+      Message toSend = new Message(MessageType.UPLOAD, stickyHeader, sHHash);
     
-    return myClient.sendMessage(toSend);
+      return client.sendMessage(toSend);
+    } 
+    else return false;
   } 
 
- /**
-  * Sends a delete request message along with the hash
-  * of the data object the user wants deleted
-  */
- public boolean deleteData(int hash) {
-   Message toSend = new Message(MessageType.DELETE, hash);
-   return myClient.sendMessage(toSend);
- } 
+  /**
+   * Sends a delete request message along with the hash
+   * of the data object the user wants deleted
+   */
+  public boolean deleteData(int hash) {
+    if (client != null) {
+      Message toSend = new Message(MessageType.DELETE, hash);
+      return client.sendMessage(toSend);
+    } 
+    else return false;
+  } 
 
   public static void main(String[] args) {
     Policy policy = new Policy(true, true);
     User user = new User(policy);
+    //Start servers
+    try {
+      int serverPort = Integer.parseInt(args[0]);
+      String cliAddress = args[1];
+      int cliPort = Integer.parseInt(args[2]);
+      user.startServers(serverPort, cliAddress, cliPort);
+    } catch (NumberFormatException n) {
+      System.out.println(n);
+    }
     //Upload user data
     boolean sent = user.uploadData("This is some data...");
     if(sent) System.out.println("Data sent successfully!");
